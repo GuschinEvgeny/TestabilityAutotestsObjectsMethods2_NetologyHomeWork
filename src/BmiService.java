f6f7291ce0f601a3866990bbd395896feb68c583
Guschin Evgeny <eg@vidis.tech>
2023-09-18 17:26:17 +0300

    add CaMeL case and change calculation method

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public int calculate ( double m , double kg) {
-        double i = kg / ( m * m ) ;
-        int index = (int) i ;
-        return index;
+    public int calculate(double height, double weight) {
+        double index = (weight / Math.pow(height, 2));
+        int preBmi = (int) index;
+        return preBmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service= new BmiService();
-        double m = 1.80 ;
-        double kg = 95 ;
-        int bmi = service.calculate( m , kg);
+        BmiService service = new BmiService();
+        int bmi = service.calculate(1.87 , 98);
         System.out.println("Ваш индекс массы тела");
         System.out.println("равен " + bmi);
     }
